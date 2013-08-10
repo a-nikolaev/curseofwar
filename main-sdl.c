becc134cb203d1e12b46052e480b0b52d24dcd9d
Alexey Nikolaev <orthofish@gmail.com>
2013-08-10 15:19:32 -0400

    no SDL_AUDIO initialization

--- main-sdl.c
+++ main-sdl.c
@@ -247,7 +247,7 @@ int main(int argc, char *argv[]) {
   screen_width = MAX (screen_width, TILE_WIDTH + 75*TYPE_WIDTH + TILE_WIDTH);
 
   /* Init SDL */
-  if ( SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0 ) {
+  if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
     fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
     exit(1);
   }
